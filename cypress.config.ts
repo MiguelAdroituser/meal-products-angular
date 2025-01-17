e3de14154fa83382098957dd19b2f01e3feccbd8
MiguelAdroituser <miguel@adroitoverseas.com>
2025-01-17 12:24:55 -0700

    use cypress-io library

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,11 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:4200',
+    retries: {
+      runMode: 5,
+      openMode: 0
+    },
+    defaultCommandTimeout: 60000, // 60 seconds to wait for commands
     testIsolation: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
